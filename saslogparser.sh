78e33e77417d5515d60e791389259032e4ebd24f
Song Qiang <songqiang@users.noreply.github.com>
2014-08-23 09:35:15 -0700

    script to highlight errors and implicit warnings

---
+++ saslogparser.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+# search for error and warning messages from SAS log
+# Song, Qiang <keeyang@ustc.edu> 2014
+#
+# with valuable inputs from Andre de Souza: http://pages.stern.nyu.edu/~adesouza/sasfinphd/index/node77.html
+
+if [ $# == 0 ];
+then
+    echo "Usage: saslogparser.sh logfile";
+    exit 0;
+fi
+
+egrep --color --line-number --ignore-case \
+    "\(ERROR\|WARNING\|uninitialized\|not found\|overwritten\|no observations\|has more than one data set with\|have been converted\)" \
+    $1;
+    
+
+    
+

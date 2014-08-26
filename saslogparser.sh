7daf8eb754602f103d03d70e84eaa5ba0115b0ea
Song Qiang <songqiang@users.noreply.github.com>
2014-08-26 00:24:27 -0700

    add more error message

--- saslogparser.sh
+++ saslogparser.sh
@@ -22,11 +22,17 @@ egrep --color --line-number --ignore-case \
     -e "has more than one data set with" \
     -e "have been converted" \
     -e "went to a new line" \
+    -e "Missing values were generated" \
     -e "operation on missing values" \
     -e "never been referenced" \
     -e "has 0 observations" \
     -e "out of proper order" \
     -e "format was too small" \
     -e "truncated to" \
+    -e "LOST CARD" \
+    -e "not executed" \
+    -e "Multiple lengths were specified" \
+    -e "Division by zero" \
+    -e "stopped due to looping" \
     $1;
     
